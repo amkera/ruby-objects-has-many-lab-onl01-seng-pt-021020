33e6e3aa97cbe32e0af23bbf3898040b872024dc
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2021-02-25 00:27:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,16 +12,28 @@ class Song
     @@all << self
   end 
   
+<<<<<<< HEAD
   def self.all #.all is a class method that returns an array of all song instances that have been created 
+=======
+  def self.all #.all is a class methof that returns an array of all song instances that have been created 
+>>>>>>> f197882d9f950d8cf3a1be4e36d128a7b79643f8
     @@all
   end 
     
   def artist_name
+<<<<<<< HEAD
       if artist 
         self.artist.name #the song knows the name of its artist 
       else 
         nil #returns nil if the song does not have an artist 
       end
   end 
+=======
+      self.artist.name
+  end 
+    
+  
+  
+>>>>>>> f197882d9f950d8cf3a1be4e36d128a7b79643f8
 end 
 
